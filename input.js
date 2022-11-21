f47c3fd1c0bc795305bb7fdaa95a168d3b9b7b8b
Noob_Hacker <gauravjais4491@gmail.com>
2022-11-21 15:28:08 +0530

    validation in contact

--- input.js
+++ input.js
@@ -569,3 +569,19 @@ function search() {
 function reset() {
     location.reload();
 }
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
